5b630c6c7894a6c9e16a0fbf88d755bd2919d44f
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2021-04-19 20:12:40 -0300

    Menu de Questoes C++
    Questao bonus c++

---
+++ Questão bonus.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+
+using namespace std;
+
+void menu()
+{
+	int c;
+		cout << "Por favor digite a questao. Caso queira sair digite 9: "<< "\n";	
+		cin >> c;
+		switch (c)
+		{
+			case 1:
+				cout << "Insira dois números para comparar: "<< "\n";
+				int x,y;
+				cin >> x;
+				cin >> y;
+			{
+				if (x == y)
+			{
+					cout << x << " é igual a "<< y;
+			}
+				else if (x > y)
+			{
+					cout << x << " é maior que "<< y ;
+			}
+				else 
+			{
+					cout << x << " é menor que " << y;
+			}	
+				menu ();
+				break;
+			}
+			
+			case 2:
+			{
+				int n;
+				cout << "Insira um número que deseja realizar a tabuada (0-20): "<< "\n";
+				cin >> n;
+				while (0<n && n<=20)
+		{
+					cout<< "--------------------------------"<< "\n";
+					cout<< n << " x "<< 1 << "  = "<< (n*1)<< "\n";
+					cout<< n << " x "<< 2 << "  = "<< (n*2)<< "\n";
+					cout<< n << " x "<< 3 << "  = "<< (n*3)<< "\n";
+					cout<< n << " x "<< 4 << "  = "<< (n*4)<< "\n";
+					cout<< n << " x "<< 5 << "  = "<< (n*5)<< "\n";
+					cout<< n << " x "<< 6 << "  = "<< (n*6)<< "\n";
+					cout<< n << " x "<< 7 << "  = "<< (n*7)<< "\n";
+					cout<< n << " x "<< 8 << "  = "<< (n*8)<< "\n";
+					cout<< n << " x "<< 9 << "  = "<< (n*9)<< "\n";
+					cout<< n << " x "<< 10 << " = "<< (n*10)<< "\n";
+					cout<< "--------------------------------";
+				break;
+				if (n<0 || n>20)
+					cout << "Insira um número válido!"<< "\n";
+		}
+				menu ();
+				break;
+		}
+			
+			
+			case 3:
+			{
+				string frase = "Não irei passar na prova";
+				frase.erase(0,3);
+				cout << frase;
+				menu ();
+				break;
+			}
+			
+			case 4:
+			{
+				int n;
+				for (n % 2== 0;n<=100;n+=2)
+					cout << n << "\n";
+				menu ();
+				break;
+			}
+			case 5:
+			{
+				int n;
+				cout<< "Insira um número para conferir se é par ou ímpar: "<< "\n";
+				cin >> n;
+				if (n=0 || n % 2==0)	
+		{
+					cout << "O número escolhido é par!"<< "\n";
+		}
+				else 
+		{
+					cout << "O número escolhido é ímpar!"<< "\n";
+		}
+				menu ();
+				break;
+		}
+			case 6:
+			{
+				float n1,n2,n3,media;
+				cout << "Insira as suas três notas, de 1 a 3 respectivamente: " << "\n";
+	
+				cin >> n1;
+				cin >> n2;
+				cin >> n3;
+				media = (n1*1 + n2*3 + n3*6)/10;
+				cout << "Sua média é: " << media << "\n";
+	
+				if (0 < media && media < 2){
+    				cout << "Sua menção é II!"<< "\n";
+    				cout << "Você reprovou!";
+		}
+				if (2 <= media && media < 5){
+    				cout << "Sua menção é MI!"<< "\n";
+    				cout << "Você reprovou!";
+		}
+				if (5 <= media && media< 7){
+    				cout << "Sua menção é MM!"<< "\n";
+    				cout << "Você reprovou!";
+ 		} 
+				if (7 <= media && media < 9){
+					cout << "Sua menção é MS!"<< "\n";
+    				cout << "Você foi aprovado!";
+		}
+				if (9 <= media){
+					cout << "Sua menção é SS!"<< "\n";
+    				cout << "Você foi aprovado!";
+				menu ();
+				break;
+		}
+		}
+			case 7:
+			{
+				int n,n1;
+				cout << "Insira dois números para realizar as operações básicas: " << "\n";
+				cin >> n;
+				cin >> n1;
+				cout << "A soma dos números é: "<< (n + n1) << "\n";
+				cout << "A subtração dos números é: "<< (n - n1) << "\n";
+				cout << "A multiplicação dos números é: "<< (n * n1) << "\n";	
+				cout << "A divisão dos números é: "<< (n/n1) << "\n"; 
+				menu ();
+				break;
+		}
+			case 8:
+		{
+				string nome;
+				int ano;
+				cout << "Me informe seu nome e ano em que nasceu:"<< "\n";
+    			cin >> nome;
+    			cin >> ano;
+				cout << "Seu nome é "<< nome << " e você tem " << (2021 - ano) << " anos!"<< "\n";
+				menu ();
+				break;
+		}			
+			
+			default:
+			{
+				cout << "Digite um numero valido!" << "\n";
+				menu ();
+				break;
+			}
+		
+}
+}
+int main(){
+	menu ();
+}
+
+
+
